15ac3ec041e5fbb0117c6ecc9ff2826055dfd79a
sumitrai <developer2607@gmail.com>
2023-03-26 15:29:52 +0530

    All Required class Added

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,5 +1,7 @@
-public class Main {
+public class MainPage {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
     }
 }
---
+++ src/NewCustomerClass.java
@@ -0,0 +1,2 @@
+public class NewCustomerClass {
+}
