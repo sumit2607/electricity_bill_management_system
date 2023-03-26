6f4f24434a4844d1561e61cef9a7026f955d9ed6
sumitrai <developer2607@gmail.com>
2023-03-26 15:58:14 +0530

    All Required class Added

--- src/AboutUs.java
+++ src/AboutUs.java
@@ -1,2 +1,54 @@
-public class AboutUs {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AboutUs extends JFrame implements ActionListener {
+
+    JButton b1;
+    JLabel j1;
+    Font f, f1, f2;
+    TextArea t1;
+    String s;
+
+
+    public AboutUs() {
+        setLayout(null);
+        JButton b1 = new JButton();
+        b1.setBounds(180, 430, 120, 200);
+        b1.addActionListener(this);
+
+        Font f = new Font("RALEWAY", Font.BOLD, 180);
+        setFont(f);
+
+        s = "#Problem -:" +
+                " The manual system is suffering from a series of drawbacks. Since whole of the bills is to be maintained with hands " +
+                "the process of keeping and maintaining the information is very tedious and lengthy to customer. It is very time consuming " +
+                "and laborious process because, staff need to be visited the customers place every month to give the bills and to receive the payments" +
+                " For this reason, we have provided features Present system is partially " +
+                "automated (computerized), existing system is quite laborious as one must enter same" +
+                " information at different places.\n" +
+                "\n" +
+                "#Solution -: This project system excludes the need of maintaining paper " +
+                "electricity bill as all the electricity bill records are managed electronically. " +
+                "Administrator doesn't have to keep a manual track of the users. " +
+                "The system automatically calculates fine. Users don't have to visit to the office for " +
+                "bill payment. There is no need of delivery boy for delivery bills to user's place. Thus, " +
+                "it saves human efforts and resources.";
+
+
+        TextArea t1 = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
+        t1.setEditable(false);
+        t1.setBounds(20, 100, 450, 300);
+        add(t1);
+
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
